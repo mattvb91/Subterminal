400bf94776f05cc80f78f5c294cd7a3ba7b9131e
Matthias von Bargen <matt@mavon.ie>
2016-12-29 21:05:17 +0000

    adview on recyclerview

--- app/src/main/java/mavonie/subterminal/Skydive/ViewAdapters/DropzoneRecycler.java
+++ app/src/main/java/mavonie/subterminal/Skydive/ViewAdapters/DropzoneRecycler.java
@@ -6,55 +6,103 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import mavonie.subterminal.Models.Skydive.Dropzone;
 import mavonie.subterminal.Models.Skydive.DropzoneAircraft;
 import mavonie.subterminal.R;
 import mavonie.subterminal.Utils.BaseFragment;
+import mavonie.subterminal.Utils.Subterminal;
 
 /**
  * Suit recycler
  */
 public class DropzoneRecycler extends RecyclerView.Adapter<DropzoneRecycler.ViewHolder> {
 
-    private final List<Dropzone> mValues;
+    static final int ITEM_TYPE_MODEL = 1;
+    static final int ITEM_TYPE_AD = 2;
+
+    static final int ITEMS_PER_AD = 15;
+
+    private final List<Object> mValues;
     private final BaseFragment.OnFragmentInteractionListener mListener;
 
-    public DropzoneRecycler(List<Dropzone> items, BaseFragment.OnFragmentInteractionListener listener) {
-        mValues = items;
+    public DropzoneRecycler(List<Object> items, BaseFragment.OnFragmentInteractionListener listener) {
+
+        if (!Subterminal.getUser().isPremium()) {
+            List<Object> updatedList = new ArrayList<>();
+            for (int i = 0; i < items.size(); i++) {
+                if (i % ITEMS_PER_AD == 0) {
+                    updatedList.add(null);
+                }
+                updatedList.add(items.get(i));
+            }
+            mValues = updatedList;
+        } else {
+            mValues = items;
+        }
+
         mListener = listener;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_dropzone, parent, false);
+
+        View view = null;
+
+        if (viewType == ITEM_TYPE_MODEL) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.fragment_dropzone, parent, false);
+        } else {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_ad_item, parent, false);
+        }
+
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.mItem = mValues.get(position);
-        holder.listName.setText(holder.mItem.getName());
-        holder.listCountry.setText(holder.mItem.getCountry());
-        holder.listAircraft.setText(new DropzoneAircraft().count(holder.mItem.aircraftParemeters()) + " Aircraft");
+        if (getItemViewType(position) == ITEM_TYPE_MODEL) {
+            holder.mItem = (Dropzone) mValues.get(position);
+            holder.listName.setText(holder.mItem.getName());
+            holder.listCountry.setText(holder.mItem.getCountry());
+            holder.listAircraft.setText(new DropzoneAircraft().count(holder.mItem.aircraftParemeters()) + " Aircraft");
 
-        holder.mView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
-                    mListener.onFragmentInteraction(holder.mItem);
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (null != mListener) {
+                        // Notify the active callbacks interface (the activity, if the
+                        // fragment is attached to one) that an item has been selected.
+                        mListener.onFragmentInteraction(holder.mItem);
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            AdView adview = (AdView) holder.mView.findViewById(R.id.adView);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            adview.loadAd(adRequest);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if (!Subterminal.getUser().isPremium()) {
+            if (mValues.get(position) == null)
+                return ITEM_TYPE_AD;
+        }
+
+        return ITEM_TYPE_MODEL;
     }
 
     @Override
     public int getItemCount() {
-        return mValues.size();
+        return mValues.size() + (mValues.size() / 15);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
